56162d3e6537a62346e674f037b7e854863ce551
ArcticZeroo <spencer@frozor.io>
2018-03-29 14:29:23 -0400

    Add EventEmitter#once, base entire site around a single page that loads others

--- js/global.js
+++ js/global.js
@@ -1,6 +1,8 @@
 // Constants
 const BAR_ADD_TIMEOUT = 50;
 const MAX_BAR_VALUE = 1;
+const PAGE_ROOT = 'https://frozor.io/netneutrality/html/';
+const HOME_URL = PAGE_ROOT + 'home';
 
 class EventEmitter {
     constructor() {
@@ -37,6 +39,15 @@ class EventEmitter {
         this._validate(event);
 
         this._addListener(event, callback);
+    }
+
+    once(event, callback) {
+        const singleEvent = () => {
+            this._listeners[event].remove(singleEvent);
+            callback();
+        };
+
+        this._addListener(event, singleEvent);
     }
 
     removeListener(event, callback) {
@@ -79,14 +90,17 @@ class PageProgressBar extends EventEmitter {
         super();
 
         this.progress = 0;
+        this.speedMultiplier = 1.0;
     }
 
     addTime() {
         if (this.progress >= MAX_BAR_VALUE) {
+            this.element.style.opacity = 0.0;
+            this.emit('done');
             return;
         }
 
-        const add = (Math.random() / 4) + 0.05;
+        const add = ((Math.random() / 4) + 0.05) * this.speedMultiplier;
 
         this.progress = Math.min(this.progress + add, MAX_BAR_VALUE);
 
@@ -98,18 +112,49 @@ class PageProgressBar extends EventEmitter {
     }
 
     start() {
-        this.element = document.getElementById('page-progress');
-
         if (!this.element) {
-            throw new Error('Progress bar was not found');
+            this.element = document.getElementById('page-progress');
+
+            if (!this.element) {
+                throw new Error('Progress bar was not found');
+            }
         }
+
+        this.progress = 0;
+        this.element.style.opacity = 1.0;
 
         setTimeout(() => this.addTime(), BAR_ADD_TIMEOUT * 2);
     }
 }
 
+const homeButton = $('#home-button');
 const progressBar = new PageProgressBar();
+const events = new EventEmitter();
+
+function loadPage(url, speed = 1.0) {
+    progressBar.speedMultiplier = speed;
+
+    progressBar.once('done', function () {
+        loadPageActual(url);
+    });
+
+    progressBar.start();
+}
+
+function loadPageActual(url) {
+    $('#webpage-iframe').attr('src', url);
+
+    events.emit('page', url);
+}
+
+events.on('page', function (url) {
+    if (url === HOME_URL) {
+        homeButton.hide();
+    } else {
+        homeButton.show();
+    }
+});
 
 $(document).ready(function () {
-    progressBar.start();
+    loadPage(HOME_URL, 3);
 });
