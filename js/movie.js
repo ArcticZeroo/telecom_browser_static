359b0220c3666dfe72c9db1675c684356c4246dc
ArcticZeroo <spencer@frozor.io>
2018-04-03 13:03:14 -0400

    add error class to movie

--- js/movie.js
+++ js/movie.js
@@ -1,3 +1,3 @@
 $('.movie-item').click(function () {
-    new PopupModal({ title: 'Why would you click that?', text: 'No really why?', id: 'music-click-bad' }).modal.addClass('red');
+    new PopupModal({ title: 'Why would you click that?', text: 'No really why?', id: 'music-click-bad', classes: ['error'] });
 });
